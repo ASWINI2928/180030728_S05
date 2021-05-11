0b5f4edb78e408fe272f8b9c7ead7b8822aefc49
ASWINI2928 <58809374+ASWINI2928@users.noreply.github.com>
2021-05-11 12:42:52 +0530

    Array Problems

---
+++ ArrayProblems/src/array/Array4.java
@@ -0,0 +1,14 @@
+package array;
+import java.util.*;
+public class Array4 {
+public static void main(String args[])
+{
+   int arr[]= {80,114,105,121,97};
+		for(int i:arr)
+		{
+			System.out.println((char)i);
+		}
+	}
+
+}
+
---
+++ ArrayProblems/src/array/Array6.java
@@ -0,0 +1,14 @@
+package array;
+import java.util.*;
+public class Array6 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int arr[]= {6,12,123,111,2,222};
+		Arrays.sort(arr);
+		for(int i:arr)
+		{
+			System.out.print(i+" ");
+		}
+	}
+}
