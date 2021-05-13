db8924890a9a674f89429a1f16a0a85fe677cfd2
ASWINI2928 <58809374+ASWINI2928@users.noreply.github.com>
2021-05-13 09:20:17 +0530

    Exception

---
+++ Session 6/src/session6/Exceptionhandling.java
@@ -0,0 +1,15 @@
+package session6;
+
+public class Exceptionhandling {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("hi");
+         try {
+         int a=6/0;//exception object
+         }
+         catch(Exception e) {}
+         System.out.println("hello");
+	}
+
+}
