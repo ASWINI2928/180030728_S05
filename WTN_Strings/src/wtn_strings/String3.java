6659d08c9bf98486498f7923ba755d70debbb111
ASWINI2928 <58809374+ASWINI2928@users.noreply.github.com>
2021-05-22 12:50:53 +0530

    Strings

---
+++ WTN_Strings/src/wtn_strings/String3.java
@@ -0,0 +1,18 @@
+package wtn_strings;
+public class String3 {
+	public static void main(String[] args) {
+		String str="Wipro";
+		int n=str.length();
+		String repeater = "";
+		if(n<2) 
+			repeater=str;
+		else 
+			repeater=str.substring(0, 2);
+		String output = "";
+		for(int i=0;i<n;i++) 
+		{
+			output+=repeater;
+		}
+		System.out.println(output);
+	}
+}
